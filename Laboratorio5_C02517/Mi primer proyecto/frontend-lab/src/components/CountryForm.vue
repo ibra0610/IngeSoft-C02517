3d29b5734bd7c80d0bb427b64aabe072b043a5e6
Javier <ibrahimcruz06@gmail.com>
2026-04-10 17:33:44 -0600

    Laboratorio #5: Se crea formulario para agregar paises

--- Laboratorio5_C02517/Mi primer proyecto/frontend-lab/src/components/CountryForm.vue
+++ Laboratorio5_C02517/Mi primer proyecto/frontend-lab/src/components/CountryForm.vue
@@ -1,15 +1,66 @@
 <template>
-    <div>
-        <h1>Formulario para crear un país</h1>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="card p-4 shadow" style="max-width: 400px; width: 100%">
+            <h3 class="text-center">Formulario de creación de países</h3>
+            <form @submit.prevent="saveCountry">
+                <div class="form-group">
+                    <label for="nombre">Nombre:</label>
+                    <input
+                        v-model="formData.Name"
+                        type="text"
+                        id="name"
+                        class="form-control"
+                        required
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="continente">Continente:</label>
+                    <select
+                        v-model="formData.Continent"
+                        id="continente"
+                        required
+                        class="form-control"
+                    >
+                        <option value="" disabled>Seleccione un continente</option>
+                        <option>África</option>
+                        <option>Asia</option>
+                        <option>Europa</option>
+                        <option>América</option>
+                        <option>Oceanía</option>
+                        <option>Antártida</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="idioma">Idioma:</label>
+                    <input
+                        v-model="formData.Language"
+                        type="text"
+                        id="idioma"
+                        class="form-control"
+                        required
+                    />
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-success btn-block">
+                        Guardar
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    setup () {
-        
-
-        return {}
+    data () {
+        return {
+            formData: { Name: "", Continent: "", Language: "" },
+        };
+    },
+    methods: {
+        saveCountry() {
+            console.log("Datos a guardar:", this.formData);
+        },
     },
 };
 </script>
