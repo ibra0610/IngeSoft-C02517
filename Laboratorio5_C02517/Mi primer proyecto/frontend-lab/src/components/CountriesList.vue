bd16f6c27437d370d9597f4824ef9f7d43d165d8
Javier <ibrahimcruz06@gmail.com>
2026-04-10 14:41:49 -0600

    Laboratorio #5: Se agregan archivos de laboratorio 3 y 4

---
+++ Laboratorio5_C02517/Mi primer proyecto/frontend-lab/src/components/CountriesList.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container mt-5"> 
+        <h1 class="display-4 text-center">Lista de países</h1> 
+        <table 
+            class="table is-bordered is-striped is-narrow is-hoverable 
+is-fullwidth" 
+> 
+    <thead> 
+        <tr> 
+            <th>Nombre</th> 
+            <th>Continente</th> 
+            <th>Idioma</th> 
+            <th>Acciones</th> 
+        </tr> 
+    </thead> 
+    <tbody> 
+        <tr v-for="(country, index) of countries" :key="index"> 
+            <td>{{country.name}}</td> 
+            <td>{{country.continent}}</td> 
+            <td>{{country.language}}</td> 
+            <td> 
+                <button class="btn btn-secondary btn-sm">Editar</button> 
+                <button class="btn btn-danger btn-sm" v-on:click="removeCountry(index)">Eliminar</button> 
+            </td> 
+        </tr> 
+    </tbody> 
+    </table> 
+</div>
+</template>
+
+<script>
+    export default {
+        name: "CountriesList",
+        data() { 
+            return { 
+                countries: [ 
+                { name: "Costa Rica", continent: "América", language: "Español"}, 
+                { name: "Japón", continent: "Asia", language: "Japonés" }, 
+                { name: "Corea del Sur", continent: "Asia", language: "Coreano" },
+                { name: "Italia", continent: "Europa", language: "Italiano" }, 
+                { name: "Alemania", continent: "Europa", language: "Alemán" }, 
+                ], 
+            }; 
+        },
+        methods: {
+            removeCountry(index) {
+                this.countries.splice(index, 1);
+                // de https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
